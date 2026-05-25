9659e584c053fe298449b5c2dd343baea776779c
ochelott <benjash8@outlook.com>
2026-05-25 15:17:19 -0300

    Ejercicio 1 y 2

---
+++ Ejercicio 1  y 2/ConsoleApp1/Ejercicio 2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Ejercicio_2
+    {
+        public static int acum = 0;
+        public static int cont = 0;
+        public static void IngresarNumero(int num)
+        {
+            acum = acum + num;
+            cont++;
+        }
+        public static double CalcularPromedio()
+        {
+            double Promedio = 0;
+            if (cont > 0)
+            {
+                Promedio = 1.0 * acum / cont;
+
+            }
+            return Promedio;
+        }
+        public static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Titulo 1");
+            Console.WriteLine("1 - Ingrese nro");
+            Console.WriteLine("2 - Calcular y Mostrar Promedio");
+            Console.WriteLine("Otro - Salir");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            return opcion;
+        }
+        public static void MostrarPantallaIngresarNumero()
+        {
+            Console.Clear();
+            Console.WriteLine("Ingrese el nro");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Ejercicio_2.IngresarNumero(num);
+        }
+        public static void MostrarPantallaCalcularYMostrarPromedio()
+        {
+            Console.Clear();
+            Console.WriteLine("Titulo Pantalla 2");
+            double promedio = Ejercicio_2.CalcularPromedio();
+            Console.WriteLine($"Promedio: {promedio}");
+            Console.WriteLine("Presione un tecla");
+            Console.ReadKey();
+        }
+    }
+}
+    
+    
+
---
+++ Ejercicio 1  y 2/ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    namespace Ejercicio_1
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                #region Inicializar variables
+                int opcion; int acum = 0; int cont = 0;
+                #endregion
+
+                #region Iterar opciones
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Titulo 1");
+                    Console.WriteLine("1 _ Ingresar Nro");
+                    Console.WriteLine("2 _ Calcular y mostrar promedio");
+                    Console.WriteLine("Otro - Salir");
+                    opcion = Convert.ToInt32(Console.ReadLine());
+#endregion
+
+                    #region Verificar opciones y ejecutar
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.Clear();
+                            Console.WriteLine("Ingresar el nro");
+                            int num = Convert.ToInt32(Console.ReadLine());
+                            acum += num;
+                            cont++;
+                            break;
+                        case 2:
+                            Console.Clear();
+                            Console.WriteLine("Titulo pantalla 2");
+                            double promedio = 0;
+                            if (cont > 0)
+                            {
+                                promedio = acum / cont;
+                                Console.WriteLine($"Promedio: {promedio:F2}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("No hubo ingresos");
+                            }
+                            Console.WriteLine("Presione una tecla para volver");
+                            Console.ReadKey();
+                            break;
+                        default:
+                            opcion = -1;
+                            break;
+                    }
+                    #endregion
+                    #region Salir o continuar
+                } while (opcion != -1);
+                Console.WriteLine("Presionar una tecla para finalizar");
+                Console.ReadKey();
+                #endregion
+            }
+        }
+    }
+}
